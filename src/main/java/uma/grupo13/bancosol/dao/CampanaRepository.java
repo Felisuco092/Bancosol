ad7282a8e26548aa0308ef346f8979b7939a2bb7
Felisuco092 <felisuco146@gmail.com>
2026-04-21 14:15:16 +0200

    Remove all Java classes, JSP files, CSS styles, and configuration files related to the Bancosol application, effectively deleting the entire project structure.

--- src/main/java/uma/grupo13/bancosol/dao/CampanaRepository.java
+++
@@ -1,8 +0,0 @@
-package uma.grupo13.bancosol.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uma.grupo13.bancosol.entity.CampanaEntity;
-
-
-public interface CampanaRepository extends JpaRepository<CampanaEntity, Integer> {
-}
