a7022c114ad3317e258d2d425a490a5fa893862d
Joalts8 <jorgenexus8@gmail.com>
2026-04-28 14:11:42 +0200

    Gets listos pero faltan jsp y los atributos

--- Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/controller/BandejaController.java
+++ Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/controller/BandejaController.java
@@ -1,22 +1,27 @@
 package uma.grupo13.bancosol.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uma.grupo13.bancosol.utils.ValidaSesion;
 
 @Controller
 @RequestMapping("/bandeja")
 public class BandejaController {
     @GetMapping("/")
-    public String doBandeja(Model model) {
+    public String doBandeja(Model model, HttpSession session) {
+        if (!ValidaSesion.verificarSesion(session)) return "redirect:/";
+
         model.addAttribute("paginaActual", "bandeja");
         return "bandeja";
     }
 
     @PostMapping("/mensaje")
-    public String doMensaje(Model model) {
+    public String doMensaje(Model model, HttpSession session) {
+        if (!ValidaSesion.verificarSesion(session)) return "redirect:/";
         return "";
     }
 
--- Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/controller/ColaboradoresController.java
+++ Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/controller/ColaboradoresController.java
@@ -1,37 +1,45 @@
 package uma.grupo13.bancosol.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uma.grupo13.bancosol.utils.ValidaSesion;
 
 @Controller
 @RequestMapping("/colaboradores")
 public class ColaboradoresController {
     @GetMapping("/")
-    public String doColaboradores(Model model) {
+    public String doColaboradores(Model model, HttpSession session) {
+        if (!ValidaSesion.verificarSesion(session)) return "redirect:/";
+
         model.addAttribute("paginaActual", "colaboradores");
         return "colaboradores";
     }
 
     @PostMapping("/editar")
-    public  String doEditarColaboradores(Model model) {
+    public  String doEditarColaboradores(Model model, HttpSession session) {
+        if (!ValidaSesion.verificarSesion(session)) return "redirect:/";
         return "";
     }
 
     @PostMapping("/crear")
-    public  String doCrearColaboradores(Model model) {
+    public  String doCrearColaboradores(Model model, HttpSession session) {
+        if (!ValidaSesion.verificarSesion(session)) return "redirect:/";
         return "";
     }
 
     @PostMapping("/borrar")
-    public  String doBorrarColaboradores(Model model) {
+    public  String doBorrarColaboradores(Model model, HttpSession session) {
+        if (!ValidaSesion.verificarSesion(session)) return "redirect:/";
         return "";
     }
 
     @PostMapping("/guardar")
-    public  String doGuardarColaboradores(Model model) {
+    public  String doGuardarColaboradores(Model model, HttpSession session) {
+        if (!ValidaSesion.verificarSesion(session)) return "redirect:/";
         return "redirect:/colaboradores";
     }
 }
