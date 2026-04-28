b3f14269b0874b798399d2910577b759f5b0cd39
Felisuco092 <felisuco146@gmail.com>
2026-04-28 18:17:49 +0200

    Hecho fetch async para campanas.html

---
+++ FrontEnd1/nuevo/js/utils/fetch.js
@@ -0,0 +1,13 @@
+function fetch_data(ruta, error) {
+    return fetch('http://localhost:3001/' + ruta)
+        .then((response) => response.json())
+        .then(data => {
+            return data
+        })
+        .catch(err => {
+            console.error("Ha ocurrido un error en el fetch:\n error")
+        })
+
+}
+
+export {fetch_data}
