b7f82b98add018481141c082295da5679f45965e
Joalts8 <jorgenexus8@gmail.com>
2026-04-28 13:23:25 +0200

    Controler base listo(faltan datos y jsp para probar)

--- Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/controller/ControllerBase.java
+++ Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/controller/ControllerBase.java
@@ -6,49 +6,67 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import uma.grupo13.bancosol.dao.CampanaRepository;
-import uma.grupo13.bancosol.entity.CampanaEntity;
+import uma.grupo13.bancosol.dao.UserRepository;
+import uma.grupo13.bancosol.entity.UsuarioEntity;
 
 import java.util.List;
 
 @Controller
 public class ControllerBase {
     @Autowired
-    protected CampanaRepository campanaRepository;
+    protected UserRepository userRepo;
 
 
     @GetMapping("/")
-    public String doStart(){
-        return "index";
+    public String doStart(HttpSession session){
+        if (verificarSesion(session)) {
+            return "redirect:/dashboard";
+        }else {
+            return "index";
+        }
     }
 
     @GetMapping("/dashboard")
-    public String doDashboard(Model model) {
-        model.addAttribute("paginaActual", "dashboard");
-        return "dashboard";
+    public String doDashboard(Model model, HttpSession session) {
+        if (verificarSesion(session)) {
+            model.addAttribute("paginaActual", "dashboard");
+            return "dashboard";
+        } else {
+            return "redirect:/";
+        }
     }
 
     @PostMapping("/login")
-    public String dologin(@RequestParam("username") String user,
-                          @RequestParam("password") String password,
-                          HttpSession session) {
+    public String dologin(@RequestParam("username") String user, @RequestParam("password") String password,
+                          HttpSession session, Model model) {
         if ("admin".equals(user) && "paswword123".equals(password)) {
             session.setAttribute("user", user);
             return "redirect:/dashboard";
         } else {
             return "redirect:/"; // de momento devuelve a la página de inicio
         }
+        /*UserEditorEntity editor = userEditorRepository.autheticate(username, password);
+        if (editor == null) {
+            model.addAttribute("error", "Usuario no encontrado o error de autenticación");
+            return "index";
+        } else {
+            session.setAttribute("user", editor);
+            session.setAttribute("rol", editor.getRol())
+            return "redirect:/dashboard";
+        }*/
     }
 
     @PostMapping("/logout")
-    public String dologout(){
+    public String dologout(HttpSession session){
+        session.invalidate();
         return "redirect:/";
     }
 
+    protected boolean verificarSesion(HttpSession session){
+        return session.getAttribute("user") != null;
+    }
 }
-
-// Post para login, logaut-> ver en movies
+// datos en la bd
 // recargar tablas
 // post de cada boton: redirije a /get con los datos necesarios, ya esta el de bandeja
