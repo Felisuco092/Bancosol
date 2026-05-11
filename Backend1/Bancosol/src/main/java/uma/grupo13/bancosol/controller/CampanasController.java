77e6c07a3739d491d7654224767f3fb51d6d5d6d
Terrible_Tatuaje <germanpelaezgallardo@gmail.com>
2026-05-11 14:16:27 +0200

    A

--- Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/controller/CampanasController.java
+++ Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/controller/CampanasController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import uma.grupo13.bancosol.dao.CampanaRepository;
 import uma.grupo13.bancosol.entity.CampanaEntity;
 import uma.grupo13.bancosol.utils.ValidaSesion;
@@ -31,9 +32,11 @@ public class CampanasController {
     }
 
     @PostMapping("/editar")
-    public  String doEditarCampana(Model model, HttpSession session) {
+    public  String doEditarCampana(@RequestParam(value = "", required = false) Integer idCampana,
+            Model model, HttpSession session) {
         if (!ValidaSesion.verificarSesion(session)) return "redirect:/";
-        return "";
+
+        return "crear_editar/crear_editar_campana";
     }
 
     @GetMapping("/crear")
