2cfe6da4d3b028690364255b8595e0a4f998df08
Terrible_Tatuaje <germanpelaezgallardo@gmail.com>
2026-06-05 18:38:33 +0200

    Notifications by user done

--- Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/dao/NotificacionRepository.java
+++ Backend1/Bancosol/src/main/java/uma/grupo13/bancosol/dao/NotificacionRepository.java
@@ -2,12 +2,15 @@ package uma.grupo13.bancosol.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import uma.grupo13.bancosol.dto.NotificacionDTO;
 import uma.grupo13.bancosol.entity.NotificacionEntity;
 import uma.grupo13.bancosol.entity.UsuarioEntity;
 
 import java.util.List;
 
 public interface NotificacionRepository extends JpaRepository<NotificacionEntity, Integer>{
-
+    @Query("select noti from NotificacionEntity noti where noti.usuarioDestino.id = :idUsuario")
+    public List<NotificacionEntity> listarNotificacionesUsuario(@Param("idUsuario") Integer idUsuario);
 
 }
